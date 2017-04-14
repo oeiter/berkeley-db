88384bab4fd6213898d16b7fa02ac200a8c249f8
oeiter <oeiter@163.com>
2017-04-14 16:56:55 +0800

    xx

--- src/main/java/com/dbstudy/QueueUtil.java
+++ src/main/java/com/dbstudy/QueueUtil.java
@@ -6,8 +6,8 @@ public class QueueUtil {
 	public static void doInQueue(File queueDir, String queueName, int cacheSize, ActionInQueue action)throws Throwable{
 		final Queue queue = new Queue(queueDir.getPath(), queueName, cacheSize);
 		doInQueue(queue, action);
-		
 	}
+	
 	public static void doInQueue(Queue queue, ActionInQueue action)throws Throwable{
 		try{
 			action.doAction(queue);
@@ -16,6 +16,7 @@ public class QueueUtil {
 		}
 		
 	}
+	
 	public static File createTempSubdir(String string) {
 		File f = new File("E:/tempqueue/"+string);
 		if(f.exists()){
